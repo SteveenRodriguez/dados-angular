66e458db7f600169ac3a4479314aeed5a363a941
SteveenRodriguez <ss.rodriguez00021@gmail.com>
2022-10-11 22:58:55 -0500

    App de dados realizada en angular utilizando eventbiding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda = '../assets/img/dice1.png';
+  dadoDerecha = '../assets/img/dice2.png';
+
+  numeroUno: number = 1;
+  numeroDos: number = 2;
+
+  tirarDados(): void {
+    this.numeroUno = Math.round(Math.random() * 5) + 1;
+    this.numeroDos = Math.round(Math.random() * 5) + 1;
+    this.dadoIzquierda = `../assets/img/dice${this.numeroUno}.png`;
+    this.dadoDerecha = `../assets/img/dice${this.numeroDos}.png`;
+  }
 }
